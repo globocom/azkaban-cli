abf5839588f28e2dd7df539d75de12a0bc350c5b
Gustavo Alves <gustavo.alves@corp.globo.com>
2018-05-24 11:41:05 -0300

    documentando modulo azkaban_cli.api e melhorias de codigo

--- azkaban_cli/api.py
+++ azkaban_cli/api.py
@@ -1,43 +1,91 @@
+# -*- coding: utf-8 -*-
+
+"""
+azkaban_cli.api
+
+This module provides a set of requests for the Azkaban API
+"""
+
 import requests
 import logging
 
 def upload_request(session, host, session_id, project, zip_name, zip_path):
+    """Upload request for the Azkaban API
+
+    :param session: A requests session for creating the request
+    :type session: requests.Session
+    :param str host: Hostname where the request should go
+    :param str session_id: An id that the user should have when is logged in
+    :param str project: Project name on Azkaban
+    :param str zip_name: Name that zip will receive
+    :param str zip_path: Local path from zip that will be uploaded
+    :return: The response from the request made
+    :rtype: requests.Response
+    :raises requests.exceptions.ConnectionError: if cannot connect to host
+    """
+
     zip_file = open(zip_path, 'rb')
 
     response = session.post(
-        host + '/manager', 
-        data = {
-            u'session.id': session_id, 
-            u'ajax': u'upload', 
+        host + '/manager',
+        data={
+            u'session.id': session_id,
+            u'ajax': u'upload',
             u'project': project
-        }, 
-        files = {
+        },
+        files={
             u'file': (zip_name, zip_file, 'application/zip'),
         }
     )
 
-    logging.debug("Response: \n%s" % (response.text))
+    logging.debug("Response: \n%s", response.text)
 
     return response
 
 def login_request(session, host, user, password):
+    """Login request for the Azkaban API
+
+    :param session: A requests session for creating the request
+    :type session: requests.Session
+    :param str host: Hostname where the request should go
+    :param str user: The user name
+    :param str password: The user password
+    :return: The response from the request made
+    :rtype: requests.Response
+    :raises requests.exceptions.ConnectionError: if cannot connect to host
+    """
+
     response = session.post(
-        host, 
-        data = {
+        host,
+        data={
             u'action': u'login',
             u'username': user,
             u'password': password
         }
     )
 
-    logging.debug("Response: \n%s" % (response.text))
+    logging.debug("Response: \n%s", response.text)
 
     return response
 
 def schedule_request(session, host, session_id, project, flow, cron):
+    """Schedule request for the Azkaban API
+
+    :param session: A requests session for creating the request
+    :type session: requests.Session
+    :param str host: Hostname where the request should go
+    :param str session_id: An id that the user should have when is logged in
+    :param str project: Project name that contains the flow that will be scheduled on Azkaban
+    :param str flow: Flow name to be scheduled on Azkaban
+    :param str cron: Cron expression in quartz format used to schedule
+    :return: The response from the request made
+    :rtype: requests.Response
+    :raises requests.exceptions.ConnectionError: if cannot connect to host
+    """
+
     response = session.post(
         host + '/schedule',
-        data = {
+        data={
             u'session.id': session_id,
             u'ajax': u'scheduleCronFlow',
             u'projectName': project,
@@ -46,14 +94,28 @@ def schedule_request(session, host, session_id, project, flow, cron):
         }
     )
 
-    logging.debug("Response: \n%s" % (response.text))
+    logging.debug("Response: \n%s", response.text)
 
     return response
 
-def execute_request(session, host, session_id, project, flow, **kwargs):
+#TODO: Add optional parameters
+def execute_request(session, host, session_id, project, flow):
+    """Execute request for the Azkaban API
+
+    :param session: A requests session for creating the request
+    :type session: requests.Session
+    :param str host: Hostname where the request should go
+    :param str session_id: An id that the user should have when is logged in
+    :param str project: Project name that contains the flow that will be executed on Azkaban
+    :param str flow: Flow name to be executed on Azkaban
+    :return: The response from the request made
+    :rtype: requests.Response
+    :raises requests.exceptions.ConnectionError: if cannot connect to host
+    """
+
     response = session.get(
         host + '/executor',
-        params = { 
+        params={
             u'session.id': session_id,
             u'ajax': 'executeFlow',
             u'project': project,
@@ -61,6 +123,6 @@ def execute_request(session, host, session_id, project, flow, **kwargs):
         }
     )
 
-    logging.debug("Response: \n%s" % (response.text))
+    logging.debug("Response: \n%s", response.text)
 
     return response
