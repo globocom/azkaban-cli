c884f2478ba8c9ee0f64c93b1fdc31113ece2b97
Gustavo Alves <gustavo.alves@corp.globo.com>
2019-03-14 15:02:33 -0300

    Fix docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,11 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
